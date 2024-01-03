2928aa72dcb81a27ec1c89a51a115f144ae36bbf
gws1017 <gws1017@naver.com>
2024-01-03 21:02:33 +0900

    초기 커밋

---
+++ Source/ProjectH/ProjectHGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectHGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTH_API AProjectHGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
