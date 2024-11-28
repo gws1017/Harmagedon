e982a4c1814a647fe66277d4238735a45fdb0ba3
wighs33 <genji@kakao.com>
2024-11-28 23:19:31 +0900

    메뉴 구현 (장비창 버튼, 인벤토리 버튼, 설정 버튼)

---
+++ Source/ProjectH/UI/SettingMenuUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/BaseUI.h"
+#include "SettingMenuUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTH_API USettingMenuUI : public UBaseUI
+{
+	GENERATED_BODY()
+	
+};
